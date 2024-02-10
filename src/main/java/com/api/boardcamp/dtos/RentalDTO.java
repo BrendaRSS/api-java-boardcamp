5ae67d1db114ce58ab585d677130f3aa1bde8e2e
Brenda Rayane <brenda.rayane.0018@gmail.com>
2024-02-10 11:52:56 -0300

    feat: rental unit test daysRented equals zero

--- src/main/java/com/api/boardcamp/dtos/RentalDTO.java
+++ src/main/java/com/api/boardcamp/dtos/RentalDTO.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RentalDTO {
 
     @NotNull(message = "customerId cannot be null")
--- src/test/java/com/api/boardcamp/RentalUnitsTests.java
+++ src/test/java/com/api/boardcamp/RentalUnitsTests.java
@@ -1,10 +1,22 @@
 package com.api.boardcamp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.api.boardcamp.dtos.RentalDTO;
+import com.api.boardcamp.exceptions.RentalDaysException;
 import com.api.boardcamp.repositories.CustomersRepository;
 import com.api.boardcamp.repositories.GameRepository;
 import com.api.boardcamp.repositories.RentalRepository;
@@ -28,5 +40,14 @@ class RentalUnitsTests {
 	@Test
 	void givenDaysRentedEqualsZero_whenCreatingRental_thenThrowsError(){
 
+		RentalDTO rentalDTO = new RentalDTO(1L, 1L, 0);
+
+		RentalDaysException exception = assertThrows(
+			RentalDaysException.class, 
+			()-> rentalService.save(rentalDTO));
+
+		assertNotNull(exception);
+		assertEquals("daysRented must be a number greater than 0", exception.getMessage());
+		verify(rentalRepository, times(0)).save(any());
 	}
 }
